56d2535c6886301a0565b8b6bdbc0149c0703a51
Guillemj98 <guillemagro98@gmail.com>
2024-10-02 22:26:18 +0200

    Add repaso

---
+++ Repaso/src/paqueteNuevo/HolaMundo.java
@@ -0,0 +1,20 @@
+package paqueteNuevo;
+
+import java.util.Scanner;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// Esto es un comentario en Java vamos a empezar a repasar Java
+		// para clase.
+		System.out.println("Esto es un mensaje que se va a imprimir por pantalla");
+		String s = "Hola Mundo";
+		// Otra forma de imprimir un mensaje es esta, declarar una variable de tipo
+		// String
+		System.out.println(s);
+		Scanner sc = new Scanner(System.in);
+		int numeroPedir = sc.nextInt();
+
+	}
+
+}
